04bba5c3e028719a0f4dae96292ece8f0799b76e
HiDana <holicdana@gmail.com>
2018-11-13 16:53:02 +0800

    Init line chatbot with node
    - Add Exrate Api with node's request
    - Test node-schedule
    - Add server auto push msg with node-schedule

---
+++ README.md
@@ -0,0 +1,21 @@
+
+
+## how to use
+
+use nodemon open app.js
+
+`$nodemon app.js`
+
+use ngrok connect nodemon's localhost:port
+
+`./ngrok http 3000`
+
+-
+
+# history
+
+20181113
+- Init line chatbot with node
+- Add Exrate Api with node's request
+- Test node-schedule
+- Add server auto push msg with node-schedule
--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // 引用linebot SDK
 var linebot = require("linebot");
+var request = require("request");
+var schedule = require("node-schedule");
+
+const data = require("./data.json");
 
 // 用於辨識Line Channel的資訊
 var bot = linebot({
@@ -8,12 +12,50 @@ var bot = linebot({
   channelAccessToken: "DepQ/NyOzsONFlCTFBJjjh9nX71mxRzbXZj6oF807lplqS6me869P/069620OqnaxEnZGdZX4ESymXzu9Fct7mR52RDalck2v7a2MsbOFI8G4oswX95ksJ+GAfd5krJr3ISIAMMstOrcarcgp8XzLgdB04t89/1O/w1cDnyilFU="
 });
 
+const userId = "Uefbd1dd6d4fe3e159fa89d398c2eb306";
+
+let replyInfo;
+
+request("https://tw.rter.info/capi.php", function(error, res, body) {
+  // console.log("statusCode:", res && res.statusCode); // Print the response status code if a response was received
+  const USDTWD = JSON.parse(res.body).USDTWD.Exrate; //美金轉台幣
+  const USDJPY = JSON.parse(res.body).USDJPY.Exrate; //美金轉日圓
+  const USDCNY = JSON.parse(res.body).USDCNY.Exrate; //美金轉人民幣
+  const USDCAD = JSON.parse(res.body).USDCAD.Exrate; //美金轉加幣
+
+  // console.log("人民幣轉台幣", USDTWD / USDCNY);
+  // console.log("日幣轉台幣", USDTWD / USDJPY);
+  // console.log("加幣轉台幣", USDTWD / USDCAD);
+  replyInfos = [
+    { title: "人民幣 轉 台幣", ext: (USDTWD / USDCNY).toFixed(4) },
+    { title: "日幣 轉 台幣", ext: (USDTWD / USDJPY).toFixed(4) },
+    { title: "加幣 轉 台幣", ext: (USDTWD / USDCAD).toFixed(4) }
+  ];
+
+  let sendMsg = replyInfos.map(info => `${info.title} ${info.ext}`).join("\n");
+  bot.push(userId, [sendMsg]);
+});
+
+// scheduleCronstyle = () => {
+//   // schedule.scheduleJob("0 0 10 * * *", function() {
+//   schedule.scheduleJob("20 * * * * *", function() {
+//     console.log("scheduleCronstyle:" + new Date());
+
+//   });
+// };
+
+// scheduleCronstyle();
+
 // 當有人傳送訊息給Bot時
 bot.on("message", function(event) {
   // event.message.text是使用者傳給bot的訊息
-  // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
+  // 準備要回傳的內容
+  // var replyMsg = `Hello:${event.message.text}`;
+  // 透過event.reply(要回傳的訊息)方法將訊息回傳給使用者
+  console.log("replyInfo", replyInfo);
+
   event
-    .reply(event.message.text)
+    .reply(replyInfo)
     .then(function(data) {
       // 當訊息成功回傳後的處理
     })
@@ -24,5 +66,7 @@ bot.on("message", function(event) {
 
 // Bot所監聽的webhook路徑與port
 bot.listen("/linewebhook", 3000, function() {
-  console.log("[BOT已準備就緒]");
+  console.log("[BOT is ready]");
+
+  // console.log();
 });
